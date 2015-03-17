1e57e16c31111bf433c9dafb273758f985c7d478
MrPointVirgule <paul-louis@outlook.fr>
2015-03-17 14:01:45 +0100

    Photo Pokemon fix (3.2.2)
    Le mauvais pokemon était affiché

--- popup.js
+++ popup.js
@@ -59,8 +59,8 @@
 					document.getElementById('mails').innerHTML=changes[key].newValue[0]; //On les inscrit dans les cases
 					document.getElementById('solde').innerHTML=changes[key].newValue[1];
 					document.getElementById('soldeforfait').innerHTML=changes[key].newValue[2];
-					if(changes[key].newValue[6] && !isNaN(changes[key].newValue[6]) && $(".afficherPokemon").length>0) //Si on a le rang
-						document.getElementById('photo').src= "http://pokeapi.co/media/img/"+(changes[key].newValue[6]-1)+".png"; //On affiche le pokemon
+					if(changes[key].newValue[6] && !isNaN(changes[key].newValue[6]) && $("#afficherPokemon").length>0) //Si on a le rang
+						document.getElementById('photo').src= "http://pokeapi.co/media/img/"+changes[key].newValue[6]+".png"; //On affiche le pokemon
 					else
 						document.getElementById('photo').src= "http://cipcnet.insa-lyon.fr/scol/tr_eleves/"+changes[key].newValue[3]+".jpg";
 					document.getElementById('nom').innerHTML=changes[key].newValue[4];
